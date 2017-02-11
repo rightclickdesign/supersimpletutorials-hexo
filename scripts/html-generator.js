067db1edaba1e0fd27d33217c22b77ae01186a4e
Tommy Nguyen <tommy@rcd.cool>
2017-02-11 00:33:40 -0500

    add related posts section

---
+++ scripts/html-generator.js
@@ -0,0 +1,34 @@
+hexo.extend.helper.register('htmlGenerator', function(args){
+  if(!args || !args.json || args.json.length == 0)return "";
+
+  var returnHTML = "";
+
+  function generateHTML(list){
+
+    var ret = "";
+    ret += "<li class=\"" + args.class + "-item\">";
+
+    if(list.date && list.date != ""){
+        ret += '<div class="'+args.class+'-date">' + list.date + "</div>";
+    }
+
+    if(list.img && list.img != ""){
+        ret += '<div class="'+args.class+'-img">' + '<img src="'+list.img+'" />' + "</div>";
+    }
+    ret += '<div class="'+args.class+'-title"><h3><a href="' + list.path + '" title="'+ list.title +'" rel="bookmark">'+ list.title + "</a></h3></div>";
+    if(list.excerpt &&  list.excerpt != ""){
+        ret += '<div class="'+args.class+'-excerpt"><p>' + list.excerpt + "</p></div>";
+    }
+
+    ret +=  "</li>";
+    return ret;
+  }
+
+  for(var i=0; i<args.json.length; i++){
+      returnHTML += generateHTML(args.json[i]);
+  }
+
+  if(returnHTML != "")returnHTML = "<ul class=\"" + args.class + "\">" + returnHTML + "</ul>";
+
+  return returnHTML;
+});
