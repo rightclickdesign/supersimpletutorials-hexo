56dd4cddad99ca1eeb254556e76b15eefc1e5ed7
Tommy Nguyen <tommy@rcd.cool>
2017-02-11 01:21:51 -0500

    style related posts

--- scripts/html-generator.js
+++ scripts/html-generator.js
@@ -15,7 +15,7 @@ hexo.extend.helper.register('htmlGenerator', function(args){
     if(list.img && list.img != ""){
         ret += '<div class="'+args.class+'-img">' + '<img src="'+list.img+'" />' + "</div>";
     }
-    ret += '<div class="'+args.class+'-title"><h3><a href="' + list.path + '" title="'+ list.title +'" rel="bookmark">'+ list.title + "</a></h3></div>";
+    ret += '<div class="'+args.class+'-title"><h5><a href="' + list.path + '" title="'+ list.title +'" rel="bookmark">'+ list.title + "</a></h5></div>";
     if(list.excerpt &&  list.excerpt != ""){
         ret += '<div class="'+args.class+'-excerpt"><p>' + list.excerpt + "</p></div>";
     }
