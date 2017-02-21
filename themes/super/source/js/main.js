3b0b4a9b9a51357e057160022e92f90569d41e81
Tommy Nguyen <tommy@rcd.cool>
2017-02-21 12:46:51 -0500

    update totop style and function

--- themes/super/source/js/main.js
+++ themes/super/source/js/main.js
@@ -42,22 +42,14 @@
     });
 
     // To Top
-    if ($('#sidebar').length) {
-        $(document).on('scroll', function () {
-            if ($(document).width() >= 800) {
-                if(($(this).scrollTop() > toTop) && ($(this).scrollTop() > 0)) {
-                    $('#toTop').fadeIn();
-                    $('#toTop').css('left', $('#sidebar').offset().left);
-                } else {
-                    $('#toTop').fadeOut();
-                }
-            } else {
-                $('#toTop').fadeIn();
-                $('#toTop').css('right', 20);
-            }
-        }).on('click', '#toTop', function () {
-            $('body, html').animate({ scrollTop: 0 }, 600);
-        });
-    }
+    $(document).on('scroll', function () {
+        if(($(this).scrollTop() > toTop) && ($(this).scrollTop() > 0)) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    }).on('click', '#toTop', function () {
+        $('body, html').animate({ scrollTop: 0 }, 600);
+    });
 
 })(jQuery);
