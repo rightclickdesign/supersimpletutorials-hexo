6d1dab0db570fa562233e58ad0b92adc2cd7b54e
Tommy Nguyen <tommy@rcd.cool>
2017-02-21 04:45:41 -0500

    update clipboard.js

--- themes/super/source/js/activate-clipboard.js
+++ themes/super/source/js/activate-clipboard.js
@@ -1,3 +1,23 @@
 (function() {
-    new Clipboard('.btn');
+    var clipboard = new Clipboard('.btn');
+    clipboard.on('success', function(e) {
+        e.clearSelection();
+        $('.copy.btn')
+            .popup('destroy');
+        $(e.trigger)
+            .popup({
+                content: "Copied!",
+                delay: {
+                    hide: 600
+                },
+                exclusive: true,
+                position: 'top center',
+                variation: 'mini'
+            })
+            .popup('show');
+    });
+    clipboard.on('error', function(e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+    });
 })();
